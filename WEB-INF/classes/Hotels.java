00a28f2ebd14490d59444c2f24db30f5a10c9b7b
Mike Plexousakis <mike.plexousakis@gmail.com>
2019-02-24 21:49:51 +0200

    Introduce Saxon XsltTransformer

--- WEB-INF/classes/Hotels.java
+++ WEB-INF/classes/Hotels.java
@@ -1,6 +1,11 @@
 import java.io.* ;
 import javax.servlet.* ;
 import javax.servlet.http.* ;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XsltCompiler;
+import net.sf.saxon.s9api.XsltExecutable;
+import net.sf.saxon.s9api.XsltTransformer;
+import net.sf.saxon.s9api.SaxonApiException;
 
 public class Hotels extends HttpServlet {
 
@@ -29,8 +34,20 @@ public class Hotels extends HttpServlet {
         catch(Exception e){
           rooms = 0;
         }
+
         String list = hotelService.listAvailable(rooms);
-        out.println(list);
+
+
+        try {
+            Processor processor = new Processor(true);
+            XsltCompiler compiler = processor.newXsltCompiler();
+            XsltExecutable exe = compiler.compile(null);
+            XsltTransformer transformer = exe.load();
+            transformer.transform();
+            out.println(list);
+        } catch (SaxonApiException sae) {
+            out.println(sae.getMessage());
+        }
 
         out.println("</html></body>") ;
     }
